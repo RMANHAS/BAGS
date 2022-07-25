bf83d9611ae41f73c163a9017b9e0d1239d3613f
gyan <gyancv@gmail.com>
2022-07-25 21:56:35 +0530

    added comment

--- crypto/main.py
+++ crypto/main.py
@@ -1,9 +1,8 @@
 import requests
 import json
-
 api_request=requests.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&convert=USD&CMC_PRO_API_KEY=d4736c89-f405-436f-8969-e6e4380c46ce')
 api=json.loads(api_request.content)
-
+# hi 
 
 coinss=[
     {
